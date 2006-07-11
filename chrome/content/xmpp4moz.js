9d12f93ec7ec223f69c8de0c53702e6385fe2e52
Massimiliano Mirra <bard@hyperstruct.net>
2006-07-11 12:44:15 +0200

    Toolbar: shows connection status, list accounts in Tools menu, and other cleanups.
    darcs-hash:20060711104415-8cc29-7c42441ec38469f903cb12f86464a3c8535f7606.gz

--- chrome/content/xmpp4moz.js
+++ chrome/content/xmpp4moz.js
@@ -1,4 +1,6 @@
 var xmppChannel = XMPP.createChannel();
+
+// Show most recent message in toolbar
 xmppChannel.on(
     { event: 'message', direction: 'in', stanza: function(s) { return s.body && s.body.toString(); }},
     function(message) {
@@ -9,6 +11,38 @@ xmppChannel.on(
             .getElementById('xmpp-last-message')
             .value = message.stanza.body;
     });
+
+// Show connection status
+xmppChannel.on(
+    { event: 'stream', direction: 'out', state: 'open' },
+    function(stream) {
+        document
+            .getElementById('xmpp-status').hidden = false;
+    });
+xmppChannel.on(
+    { event: 'stream', state: 'close' },
+    function(stream) {
+        document
+            .getElementById('xmpp-status').hidden = true;
+    });
+
+xmppChannel.on(
+    { event: 'iq', direction: 'out', stanza: function(s) {
+            return (s.@type == 'set' &&
+                    s.*::query.length() > 0 &&
+                    s.*::query.name().uri == 'jabber:iq:auth') }},
+    function(iq) {
+        xmppChannel.on( // TODO: need one-shot listeners here!
+            { event: 'iq', direction: 'in', session: iq.session, stanza: function(s) {
+                    return s.@id == iq.stanza.@id;
+                }},
+            function(reply) {
+                document.
+                    getElementById('xmpp-status').hidden = true;
+            });
+    });
+
+
 
 function xmppToggleLivebar() {
     var sidebar = document.getElementById('livebar');
@@ -27,10 +61,8 @@ function xmppOpenTracer() {
     window.open('chrome://xmpp4moz/content/debug.xul', 'xmpp-session-tracer', 'chrome,alwaysRaised');
 }
 
-function xmppPopulateAccountMenu() {
-    var menuPopup = document.getElementById('xmpp-menu-accounts');
-
-    deleteChildren(menuPopup);
+function xmppPopulateAccountMenu(menuPopup) {
+    clearMenu(menuPopup);
     
     XMPP.accounts.forEach(
         function(account) {
@@ -44,21 +76,18 @@ function xmppPopulateAccountMenu() {
             menuItem.setAttribute('type', 'checkbox');
             if(XMPP.isUp(account.address + '/' + account.resource))
                 menuItem.setAttribute('checked', 'true');
-            menuPopup.appendChild(menuItem);            
+            menuPopup.insertBefore(menuItem, menuPopup.firstChild);            
         });
-
-    menuPopup.appendChild(document.createElement('menuseparator'));
-    var menuItem = document.createElement('menuitem');
-    menuItem.setAttribute('label', 'Add or modify...');
-    menuItem.addEventListener(
-        'command', function(event) {
-            window.openPreferences('xmpp-pane');
-        }, false);
-    menuPopup.appendChild(menuItem);   
 }
 
 // ----------------------------------------------------------------------
 // UTILITIES
+
+function clearMenu(menuPopup) {
+    while(menuPopup.firstChild &&
+          menuPopup.firstChild.nodeName != 'menuseparator')
+        menuPopup.removeChild(menuPopup.firstChild);
+}
 
 function deleteChildren(container) {
     var i = container.childNodes.length - 1;
