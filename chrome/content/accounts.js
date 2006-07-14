2e1a0403c38e9e7098a5961383ed118131e99ef1
Massimiliano Mirra <bard@hyperstruct.net>
2006-07-14 21:48:41 +0200

    Added overlay that applications can include to provide the user a menulist to choose an account from.
    darcs-hash:20060714194841-8cc29-534aab4bfd4b31f158badccc8558eafa8318da46.gz

---
+++ chrome/content/accounts.js
@@ -0,0 +1,20 @@
+window.addEventListener(
+    'load', function(event) {
+        xmppRefreshAccounts(
+            document.getElementById('xmpp-menu-accounts'));
+    }, false);
+
+function xmppRefreshAccounts(menuPopup) {
+    while(menuPopup.firstChild &&
+          menuPopup.firstChild.nodeName != 'menuseparator')
+        menuPopup.removeChild(menuPopup.firstChild);
+    
+    for each(var account in XMPP.accounts) {
+        var menuItem = document.createElement('menuitem');
+        menuItem.setAttribute('label', account.jid);
+        menuItem.setAttribute('value', account.jid);
+        if(!XMPP.isUp(account.jid))
+            menuItem.setAttribute('class', 'xmpp-offline');
+        menuPopup.insertBefore(menuItem, menuPopup.firstChild);
+    }
+}
