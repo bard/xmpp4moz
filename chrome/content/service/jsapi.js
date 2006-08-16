94df72d49b427ccab326fcff2aecd44d466fa847
Massimiliano Mirra <bard@hyperstruct.net>
2006-08-16 12:49:17 +0200

    Moved Javascript layer in file loaded by the subscript loader, so that functions can be defined in the file's top scope.
    darcs-hash:20060816104917-8cc29-090ac5efe9019b67a3c1d859640e59d8dd98229a.gz

---
+++ chrome/content/service/jsapi.js
@@ -0,0 +1,338 @@
+/**
+ * This is a convenience stateless wrapper to the bare XPCOM service,
+ * to allow using it more comfortably from javascript.
+ *
+ * Example of creating a channel that filters incoming events,
+ * bringing a session up, and sending a message.
+ *
+ *     var channel = XMPP.createChannel();
+ *     channel.on(
+ *         {event: 'message', direction: 'in'},
+ *         function(message) { alert(message.stanza); } );
+ *     
+ *     XMPP.up(
+ *         'user@server.org/Resource',
+ *         {password: 'secret'});
+ *     
+ *     XMPP.send(
+ *         'user@server.org/Resource',
+ *         <message to="contact@server.org">
+ *         <body>hello</body>
+ *         </message>);
+ *     
+ */
+
+const _xmpp = Components
+    .classes['@hyperstruct.net/xmpp4moz/xmppservice;1']
+    .getService(Components.interfaces.nsIXMPPClientService)
+    .wrappedJSObject;
+    
+const _serializer = Components
+    .classes['@mozilla.org/xmlextras/xmlserializer;1']
+    .getService(Components.interfaces.nsIDOMSerializer);
+    
+const _pref = Components
+    .classes['@mozilla.org/preferences-service;1']
+    .getService(Components.interfaces.nsIPrefService)
+    .getBranch('xmpp.');
+    
+const _prompter = Components
+    .classes["@mozilla.org/embedcomp/prompt-service;1"]
+    .getService(Components.interfaces.nsIPromptService);
+    
+this.__defineGetter__(
+    'accounts', function() {
+        var accountTable = {};
+        for each(var accountInfo in
+                 this._pref.getChildList('account.', {})) {
+            var infoParts    = accountInfo.split('.');
+            var accountIndex = infoParts[1];
+            var propertyName = infoParts[2];
+            if(!accountTable[accountIndex])
+                accountTable[accountIndex] = {};
+
+            var prefReaders = ['getCharPref', 'getIntPref', 'getBoolPref'];
+            var propertyValue;
+            for each(var reader in prefReaders) 
+                try {
+                    propertyValue = this._pref[reader](accountInfo);
+                    break;
+                } catch(e) {}
+
+            accountTable[accountIndex][propertyName] = propertyValue;
+        }
+        
+        var accountList = [];
+        for(var accountIndex in accountTable) {
+            var account = accountTable[accountIndex];
+            account.index = accountIndex;
+            account.__defineGetter__(
+                'jid', function() {
+                    return this.address + '/' + this.resource;
+                });
+            accountList.push(account);
+        }
+        
+        return accountList;
+    });
+
+function getAccountByJid(jid) {
+    for each(var account in this.accounts) {
+        if(account.jid == jid)
+            return account;
+    }
+    return null;
+}
+
+function getAccountByKey(index) {
+    // XXX inefficient
+    for each(var account in this.accounts) {
+        if(account.index == index)
+            return account;
+        
+        return null;
+    }
+}
+
+function isUp(account) {
+    return this._xmpp.isUp(
+        typeof(account) == 'object' ? account.jid : account);
+}
+
+function _promptAccount(jid, requester) {        
+    var params = {
+        requester: requester,
+        confirm: false,
+        jid: jid,
+        password: undefined
+    };
+    window.openDialog(
+        'chrome://xmpp4moz/content/ui/signon.xul',
+        'xmpp-signon', 'modal,centerscreen',
+        params);
+    return params;
+}
+
+function up(account, opts) {
+    opts = opts || {};
+
+    if(typeof(account) == 'object') {
+        if(account.jid)
+            this._up(account.jid, opts);
+        else {
+            var userContinuation = opts.continuation;                
+            opts.continuation = function(jid) {
+                account.jid = jid;
+                if(userContinuation)
+                    userContinuation(jid);
+            }
+
+            this._up(null, opts);
+        }
+    } else
+        this._up(account, opts);
+}
+
+function _up(jid, opts) {
+    opts = opts || {};
+    var connectionHost = opts.host || this.getAccountByJid(jid).connectionHost;
+    var connectionPort = opts.port || this.getAccountByJid(jid).connectionPort;
+    var ssl = opts.ssl || (this.getAccountByJid(jid).connectionSecurity == 1);
+    var password = opts.password || this.getAccountByJid(jid).password;
+    var continuation = opts.continuation;
+    var requester = opts.requester;
+
+    if(!((jid && password) ||
+         (jid && this.isUp(jid)))) {
+
+        var userInput = this._promptAccount(jid, requester);
+
+        if(userInput.confirm) {
+            password = userInput.password;
+            jid = userInput.jid;
+        }
+    }
+
+    if(this.isUp(jid) && continuation)
+        continuation(jid);
+    else if(jid && password) {
+        var xmpp = this._xmpp;
+        xmpp.open(jid, connectionHost, connectionPort, ssl);
+        var m = jid.match(/^([^@]+)@([^\/]+)\/(.+)$/);
+        var username = m[1];
+        var server   = m[2];
+        var resource = m[3];
+        this.send(
+            jid,
+            <iq to={server} type="set"><query xmlns="jabber:iq:auth">
+            <username>{username}</username>
+            <password>{password}</password>
+            <resource>{resource}</resource>
+            </query></iq>,
+            function(reply) {
+                if(reply.stanza.@type == 'result') {
+                    xmpp.send(jid, <iq type="get"><query xmlns="jabber:iq:roster"/></iq>);
+                    xmpp.send(jid, <presence/>);
+                    if(continuation)
+                        continuation();
+                }
+            });
+    }
+}
+
+// could have a reference count mechanism
+
+function down(jid) {
+    this._xmpp.close(jid);
+}
+
+function send(account, stanza, handler) {
+    var _this = this;
+    if(this.isUp(account))
+        this._send(account.jid || account, stanza, handler);
+    else
+        // TODO will multiple send cause multiple signon dialogs?
+        this.up(account, {continuation: function(jid) {
+                        _this._send(jid, stanza, handler);
+                    }});
+}
+
+function _send(jid, stanza, handler) {
+    this._xmpp.send(
+        jid, typeof(stanza) == 'xml' ? stanza.toXMLString() : stanza.toString(),
+        { observe: function(jid, topic, reply) {
+                handler(reply);
+            }});
+}
+
+function createChannel(baseFilter) {
+    var channel = {
+        _watchers: [],
+
+        // unused -- will be used for things like binding a
+        // channel to a specific session, even after the event
+        // handlers have already been defined
+            
+        _baseFilter: baseFilter,
+
+        // unused
+            
+        set baseFilter(val) {
+            this._baseFilter = val;
+        },
+
+        // unused
+
+        get baseFilter() {
+            return this._baseFilter;
+        },
+
+        // temporarily stop the channel from forwardin events to
+        // the handlers
+
+        pause: function() {
+            // stub
+        },
+
+        restart: function() {
+            // stub
+        },            
+
+        on: function(pattern, handler) {
+            this._watchers.push({pattern: pattern, handler: handler});
+        },
+
+        handle: function(event) {
+            this._handle1(event, this._watchers, this._match1);
+        },
+
+        observe: function(subject, topic, data) {
+            switch(topic) {
+                case 'stream-in':
+                case 'stream-out':
+                this.handle({
+                    event: 'stream',
+                    session: subject,
+                    direction: topic == 'stream-in' ? 'in' : 'out',
+                    state: data
+                    });
+                break;
+                case 'data-in':
+                case 'data-out':
+                this.handle({
+                    event: 'data',
+                    session: subject,
+                    direction: topic == 'data-in' ? 'in' : 'out',
+                    content: data
+                    });
+                break;
+                case 'stanza-in':
+                case 'stanza-out':
+                var stanza = new XML(data);
+                this.handle({
+                    event: stanza.name(),
+                    session: subject,
+                    direction: topic == 'stanza-in' ? 'in' : 'out',
+                    stanza: stanza
+                    });
+            }
+        },
+
+        release: function() {
+            XMPP._xmpp.removeObserver(this);
+        },
+
+        // not relying on non-local state
+
+        _handle1: function(object, watches, matcher) {
+            for each(var watch in watches) {
+                if(matcher(object, watch.pattern))
+                    watch.handler(object);
+            }
+        },
+
+        // not relying on non-local state
+
+        _match1: function(object, template) {
+            var pattern, value;
+            for(var member in template) {
+                value = object[member];
+                pattern = template[member];
+        
+                if(pattern === undefined)
+                    ;
+                else if(pattern && typeof(pattern) == 'function') {
+                    if(!pattern(value))
+                        return false;
+                }
+                else if(pattern && typeof(pattern.test) == 'function') {
+                    if(!pattern.test(value))
+                        return false;
+                }
+                else if(pattern && pattern.id) {
+                    if(pattern.id != value.id)
+                        return false;
+                }
+                else if(pattern != value)
+                    return false;
+            } 
+
+            return true;
+        },
+
+        // not relying on non-local state
+
+        _union: function(x, y) {
+            var u = {};
+            for(var name in x)
+                u[name] = x[name];
+            for(var name in y)
+                u[name] = y[name];
+            return u;    
+        }
+    }
+
+    this._xmpp.addObserver(channel);
+        
+    return channel;
+}
