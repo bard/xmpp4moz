f6262b65906b2f5120b729822abdfdd7c6025866
Massimiliano Mirra <bard@hyperstruct.net>
2006-08-16 22:18:51 +0200

    Basic support for XMPP-enabled remote non-chrome applications.
    darcs-hash:20060816201851-8cc29-1b04a5c676f98a5578706286971b7a87f2f0c55d.gz

--- chrome/content/ui/overlay.js
+++ chrome/content/ui/overlay.js
@@ -3,6 +3,74 @@
 
 var xmppChannel = XMPP.createChannel();
 
+var xmppEnabledLocations = {
+    add: function(uri, account, address, type) {
+        uri = stripUriFragment(uri);
+        
+        var location = {
+            account: account,
+            address: address,
+            type: type,
+            channel: undefined,
+            active: false
+        }
+        
+        this._locations[uri] = location;
+        return location;
+    },
+
+    remove: function(uri) {
+        uri = stripUriFragment(uri);
+        
+        this._locations[uri] = undefined;
+        delete this._locations[uri];
+    },
+
+    get: function(uri) {
+        uri = stripUriFragment(uri);
+        
+        return this._locations[uri];
+    },
+
+    has: function(uri) {
+        uri = stripUriFragment(uri);
+
+        if(this._locations[uri])
+            return true;
+    },
+
+    // INTERNALS
+
+    _locations: {}
+};
+
+
+// UTILITIES
+// ----------------------------------------------------------------------
+
+// XXX remove from global namespace or rename
+function stripUriFragment(uri) {
+    var hashPos = uri.lastIndexOf('#');
+    return (hashPos != -1 ?
+            uri.slice(0, hashPos) :
+            uri);
+}
+
+// XXX remove from global namespace or rename
+function JID(string) {
+    var m = string.match(/^(.+?)@(.+?)(?:\/|$)(.*$)/);
+    var jid = {
+        username: m[1],
+        hostname: m[2],
+        resource: m[3],
+        nick: m[3],
+        address: m[1] + '@' + m[2],
+        full: m[3] ? string : null
+    }
+
+    return jid;
+}
+
 
 // NETWORK REACTIONS
 // ----------------------------------------------------------------------
@@ -55,6 +123,75 @@ xmppChannel.on(
 // GUI ACTIONS
 // ----------------------------------------------------------------------
 
+function xmppEnableContent(account, address, type) {
+    var uri = stripUriFragment(content.document.location.href);
+    var appNS = new Namespace(uri);
+    
+    // BOOKKEEPING
+
+    xmppEnabledLocations.add(uri, account, address, type);
+    
+    content.addEventListener(
+        'unload', function() {
+            channel.release();
+            xmppEnabledLocations.remove(uri);
+        }, false);
+
+    xmppRefreshContent();
+
+    // CONTENT
+
+    function receivedContentInput(text) {
+        var message = <message to={address} type={type}/>;
+        message.text = new XML(text);             
+        XMPP.send(account, message)        
+    }
+    
+    content.document.getElementById('output').addEventListener(
+        'DOMNodeInserted', function(event) {
+            receivedContentInput(event.target.textContent);
+        }, false);
+
+    // NETWORK
+
+    var channel = XMPP.createChannel();
+
+    function receivedNetworkInput(message) {
+        if(message.stanza.appNS::x.length() > 0) {
+            var payload = message.stanza.appNS::x.*[0].toXMLString();
+            content.document.getElementById('input').textContent = payload;
+        }
+    }
+
+    channel.on({
+        event: 'message',
+        direction: 'in',
+        session: function(s) {
+                return s.name == account;
+            },
+        stanza: function(s) {
+                return (s.@type == type &&
+                        JID(s.@from).address == address &&
+                        s.appNS::x.length() > 0);
+            }},
+        function(message) { receivedNetworkInput(message); });
+}
+
+// XXX remove parameter, it will always work on current browser
+
+function xmppRefreshContent() {
+    var xmppLocation = xmppEnabledLocations.get(content.location.href);
+    var toolbox = document.getElementById('xmpp-toolbox');
+
+    if(xmppLocation) 
+        for each(var role in ['account', 'address', 'type']) {
+            toolbox.getElementsByAttribute('role', role)[0]
+                .value = xmppLocation[role];
+        }
+
+    toolbox.hidden = !xmppLocation;
+}
+
 function xmppToggleLivebar() {
     var sidebar = document.getElementById('livebar');
     var splitter = document.getElementById('livebar-splitter');
@@ -106,6 +243,29 @@ window.addEventListener(
         }
     }, false);
 
+var xmppLocationChangeListener = {
+    QueryInterface: function(aIID) {
+        if(aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+           aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+           aIID.equals(Components.interfaces.nsISupports))
+            return this;
+        throw Components.results.NS_NOINTERFACE;
+    },
+    onLocationChange: function(aProgress, aRequest, aURI) {
+        xmppRefreshContent();
+    },
+    onStateChange: function(aProgress, aRequest, aStateFlags, aStatus) {},
+    onProgressChange: function() {},
+    onStatusChange: function() {},
+    onSecurityChange: function() {},
+    onLinkIconAvailable: function() {}
+};
+
+window.addEventListener(
+    'load', function(event) {
+        gBrowser.addProgressListener(xmppLocationChangeListener);
+    }, false);
+
 
 // HOOKS
 // ----------------------------------------------------------------------
@@ -116,6 +276,4 @@ function xmppSelectedAccount(accountJid) {
     else
         XMPP.up(accountJid);
 }
-
-
 
