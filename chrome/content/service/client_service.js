99b53b78b8027388c176bb3e62b13c39f594f217
Massimiliano Mirra <bard@hyperstruct.net>
2006-08-25 20:37:06 +0200

    Service: in response to disco queries, send out basic answer (containing only <identity>).
    darcs-hash:20060825183706-8cc29-84c13f835ecd1a3bf6a41cc58e6baf1d0f81dce2.gz

--- chrome/content/service/client_service.js
+++ chrome/content/service/client_service.js
@@ -89,16 +89,32 @@ function open(jid, server, port, ssl) {
                     transport.write(
                         subject.QueryInterface(Ci.nsISupportsString).toString());
 
-                if(topic == 'stanza-in') 
-                    if(subject.nodeName == 'presence')
+                if(topic == 'stanza-in')
+                    switch(subject.nodeName) {
+                    case 'presence':
                         cache.presence.receive(
                             {session: sessions.get(data), stanza: subject});
-                    else if(subject.nodeName == 'iq' &&
-                            subject.getElementsByTagName('query')[0] &&
-                            subject.getElementsByTagName('query')[0]
-                            .getAttribute('xmlns') == 'jabber:iq:roster')
-                        cache.roster.receive(
-                            {session: sessions.get(data), stanza: subject});
+                        break;
+                    case 'iq':
+                        var query = subject.getElementsByTagName('query')[0];
+                        if(query)
+                            switch(query.getAttribute('xmlns')) {
+                            case 'jabber:iq:roster':
+                                cache.roster.receive(
+                                    {session: sessions.get(data), stanza: subject});
+                                break;
+                            case 'http://jabber.org/protocol/disco#info':
+                                var stanza =
+                                    <iq type="result" to={subject.getAttribute('from')}>
+                                    <query xmlns="http://jabber.org/protocol/disco#info">
+                                         <identity category="client" type="pc" name="xmpp4moz"/>
+                                         </query>
+                                         </iq>;
+                                session.send(stanza.toXMLString(), null);
+                                break;
+                            }
+                        break;
+                    }
 
                 if(topic == 'stream-out' &&
                    'close' == subject.QueryInterface(Components.interfaces.nsISupportsString).toString()) {
