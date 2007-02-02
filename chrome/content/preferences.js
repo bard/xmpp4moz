14a6d6aa37f0c133fca33bb060a67c688ec15218
Massimiliano Mirra <bard@hyperstruct.net>
2007-02-03 00:32:10 +0100

    Moved preference overlays at the same level of main overlays, renamed firefox_ and thunderbird_ to browser_ and mail_.
    darcs-hash:20070202233210-8cc29-638c0b864981ec1968a5d3903d9f9dfe78d6f71a.gz

--- chrome/content/preferences.js
+++ chrome/content/preferences.js
@@ -40,4 +40,4 @@ var xmppPrefs = {};
 Components
 .classes['@mozilla.org/moz/jssubscript-loader;1']
 .getService(Components.interfaces.mozIJSSubScriptLoader)
-    .loadSubScript('chrome://xmpp4moz/content/ui/preferences_impl.js', xmppPrefs);
+    .loadSubScript('chrome://xmpp4moz/content/preferences_impl.js', xmppPrefs);
