4f865515652a8a74fc938dae8fde782007d7764f
Massimiliano Mirra <bard@hyperstruct.net>
2008-03-18 01:43:03 +0100

    Service: disco#info requests weren't properly tracked (missing id attribute).  Fixed.  (Reported by Itay Neeman.)
    darcs-hash:20080318004303-8cc29-27025f56add03b8f4b020145d7b9f0b6ffa577ab.gz

--- chrome/content/service/client_service.js
+++ chrome/content/service/client_service.js
@@ -229,7 +229,8 @@ function open(jid, connector, connectionProgressObserver) {
                subject.getAttribute('type') == 'get') {
                 if(subject.getElementsByTagNameNS(ns_disco_info, 'query')[0]) {
                     var stanza =
-                        <iq type="result" to={subject.getAttribute('from')}>
+                        <iq type="result" to={subject.getAttribute('from')}
+                    id={subject.getAttribute('id')}>
                         <query xmlns="http://jabber.org/protocol/disco#info">
                         <identity category="client" type="pc" name="xmpp4moz"/>
                         <feature var="http://jabber.org/protocol/disco#info"/>
