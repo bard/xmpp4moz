db4058457dd2fb646223434487b39430b9fb0660
Massimiliano Mirra <bard@hyperstruct.net>
2009-03-10 14:01:11 +0100

    Overlay: removed progress bar and unused functions.

--- chrome/content/overlay_impl.js
+++ chrome/content/overlay_impl.js
@@ -1,23 +1,23 @@
 /*
- * Copyright 2006-2007 by Massimiliano Mirra
- * 
+ * Copyright 2006-2009 by Massimiliano Mirra
+ *
  * This file is part of xmpp4moz.
- * 
+ *
  * xmpp4moz is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * xmpp4moz is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
- *  
+ *
  */
 
 
@@ -43,114 +43,6 @@ var channel;
 
 function _(id) {
     return document.getElementById('xmpp-' + id);
-}
-
-
-// INITIALIZATION
-// ----------------------------------------------------------------------
-
-function init() {
-    if(prefBranch.getBoolPref('xmpp.firstInstall'))
-        // We used to add the toolbar button here.  No longer doing
-        // that, but keeping the check around.
-        prefBranch.setBoolPref('xmpp.firstInstall', false);
-
-    // Start watching XMPP traffic
-
-    channel = XMPP.createChannel();
-
-    // Show progress bar when waiting for connection
-
-    channel.on({
-        event: 'connector',
-        state: 'connecting'
-    }, function(stream) {
-        document
-            .getElementById('xmpp-connecting-account').value = stream.account;
-        document
-            .getElementById('xmpp-status').hidden = false;
-    });
-
-    // Hiding progress bar when transport is stopped for any reason
-
-    channel.on({
-        event: 'connector',
-        state: 'disconnected'
-    }, function() {
-        document
-            .getElementById('xmpp-status').hidden = true;
-    });
-
-    // Hiding progress bar when connector has authenticated
-
-    channel.on({
-        event: 'connector',
-        state: 'active'
-    }, function() {
-        document
-            .getElementById('xmpp-status').hidden = true;
-    });
-    
-    // Report connection error
-
-    channel.on({
-        event: 'connector',
-        state: 'error'
-    }, function(connector) {
-        document
-            .getElementById('xmpp-status').hidden = true;
-        // alert(connector.info.toXMLString());
-        // alert(connector.event);
-        // alert(connector.state);
-        // alert(XMPP.getStreamErrorCondition(connector.info));
-        // alert(XMPP.getStreamErrorMessage(XMPP.getStreamErrorCondition(connector.info)));
-        // response.stanza.error.*[0].name().localName.replace(/-/g, ' ') +
-        // ' (' + response.stanza.error.@code + ')';
-    });
-}
-
-function finish() {
-    channel.release();
-}
-
-
-// GUI ACTIONS
-// ----------------------------------------------------------------------
-
-function modifyToolbarButtons(modifier) {
-    var toolbar =
-        document.getElementById('nav-bar') ||
-        document.getElementById('mail-bar') ||
-        document.getElementById('mail-bar2');
-
-    if(!toolbar)
-        return;
-
-    if(toolbar.getAttribute('customizable') == 'true') {
-        var newSet = modifier(toolbar.currentSet);
-        if(!newSet)
-            return;
-
-        toolbar.currentSet = newSet;
-        toolbar.setAttribute('currentset', toolbar.currentSet);
-        toolbar.ownerDocument.persist(toolbar.id, 'currentset');
-        try { BrowserToolboxCustomizeDone(true); } catch (e) {}
-    }
-}
-
-function removeToolbarButton(buttonId) {
-    modifyToolbarButtons(function(set) {
-        if(set.indexOf(buttonId) != -1)
-            return set.replace(buttonId, '');
-    });
-}
-
-function addToolbarButton(buttonId) {
-    modifyToolbarButtons(function(set) {
-        if(set.indexOf(buttonId) == -1)
-            return set.replace(/(urlbar-container|separator)/,
-                               buttonId + ',$1');
-    });
 }
 
 
